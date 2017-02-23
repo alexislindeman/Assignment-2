6139ab2301d1bfdf9f344c91c60ae6ea8010a064
alexislindeman <angelalexis09@msn.com>
2017-02-22 20:34:15 -0800

    First draft

---
+++ script.js
@@ -0,0 +1,56 @@
+/*
+//Step 1
+var name = prompt("What is your name?");
+alert(name.length);
+
+
+//Step 2
+var name = prompt("What is your name?");
+var number = prompt("Now pick a number.");
+alert(name.charAt(number));
+
+
+//Step 3
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+alert("Your name is: " + firstName + lastName);
+
+
+//Step 4
+var phrase = "The quick brown fox jumps over the lazy dog";
+alert(phrase.indexOf("fox"));
+
+
+//Step 5
+var phrase = "The quick brown fox jumps over the lazy fox";
+alert(phrase.lastIndexOf("fox"));
+
+
+//Step 6
+var phrase = "The quick brown fox jumps over the lazy dog";
+var name = prompt("What is your full name?");
+alert(phrase.replace("the lazy dog", name));
+
+
+//Step 7
+var phrase = "The quick brown fox jumps over the lazy dog";
+var word = prompt("Type a word.");
+alert(phrase.search(word));
+
+
+//Step 8
+var old_string = "The quick brown fox jumps over the lazy dog";
+var new_string = (old_string.slice(30, 43));
+alert(new_string.toUpperCase());
+
+
+//Step 9
+var string = "            THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ";
+alert(string.trim().toLocaleLowerCase());
+
+
+//Step 10
+var phrase = "the quick brown fox jumps over the lazy dog";
+var capital = (phrase.slice(0).toUpperCase());
+alert(phrase);
+*/
---
+++ script2.js
@@ -0,0 +1,55 @@
+//Part 1
+var x = -10;
+console.log(Math.abs(x));
+
+
+
+/*
+var x = .6434; console.log(Math.ceil(x));
+
+var x = .6434; console.log(Math.floor(x));
+
+console.log(Math.max(10,20,30));
+console.log(Math.min(10,20,30));
+
+console.log(Math.sqrt(9));
+//Part 2
+
+
+
+//Part 3
+
+
+//Part 4
+
+
+//Part 5
+var choice = prompt ("Heads or Tails?");
+ var x = Math.random(); 
+ 
+if (x >= .5 && choice == "Heads") {
+ alert("The flip was heads and you chose heads...you win!");
+ } else if (x > .5 && choice == "Tails") {
+ alert("The flip was heads but you chose tails...you lose!");
+ } else if (x < .5 && choice == "Heads") {
+ alert("The flip was tails but you chose heads...you lose!");
+ } else if (x < .5 && choice == "Tails") {
+ alert("The flip was tails and you chose tails...you win!");
+ }
+
+//Part 6
+
+
+//Part 7
+
+
+//Part 8
+
+
+//Part 9
+var x = 0;
+while (x < 15) {
+	x++;
+	console.log(x);
+}
+console.log("I have reached the end!");
